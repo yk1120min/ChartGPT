a6258dd4e747887d532384be88ca7eccaa58c6f4
yk <yk@example.com>
2025-06-22 20:04:21 +0900

    EMA、MACD、RSIのデータをAIに渡せるようにする

--- src/main.py
+++ src/main.py
@@ -1,41 +1,68 @@
 from data_fetch.twelvedata_api import fetch_data
 from data_process.dataframe_utils import format_dataframe
-from indicators.moving_average import add_sma
-from indicators.moving_average import add_sma_slope
+from indicators.SMA import add_sma
+from indicators.SMA import add_sma_slope
 from ai.gpt_signal import get_trade_signal
 from notify.email_notifier import send_email_notification
+from indicators.EMA import add_ema
+from indicators.EMA import add_ema_slope
+from indicators.MACD import add_macd
+from indicators.RSI import add_rsi
+
 
 
 # 取得する時間足のリスト
-pairs = [("15min", 200), ("1h", 200), ("1day", 200)]
+pairs = [("15min", 200), ("1h", 200), ("4h", 200)]
 multi_df={}
 
 #データを取得
 for interval,outputsize in pairs:
-    #print(f"\n=== {interval} データ取得中 ===")
     json_data = fetch_data(symbol="USD/JPY", interval=interval, outputsize=outputsize)
 
     if json_data:
         # 取得したデータをDataFrameへ整形
         df = format_dataframe(json_data)
 
-        # テクニカル指標追加（SMAと傾き）
-        df = add_sma(df, period=20)
-        df = add_sma(df, period=50)
-        df = add_sma(df, period=100)
-        df = add_sma_slope(df, period=20)
-        df = add_sma_slope(df, period=50)
-        df = add_sma_slope(df, period=100)
+        #SMAと傾きを追加
+        #df = add_sma(df, period=20)
+        #df = add_sma(df, period=50)
+        #df = add_sma(df, period=100)
+        #df = add_sma_slope(df, period=20)
+        #df = add_sma_slope(df, period=50)
+        #df = add_sma_slope(df, period=100)
+
+        #EMAと傾きを追加
+        df = add_ema(df, period=20)
+        df = add_ema(df, period=50)
+        df = add_ema_slope(df, period=20)
+        df = add_ema_slope(df, period=50)
+
+        #MACDを追加
+        df = add_macd(df, short_period=12, long_period=26, signal_period=9)
+
+        #RSIを追加
+        add_rsi(df, period=14)
+
 
         multi_df[interval] = df
 
     else:
         print(f" {interval} データ取得失敗")
 
+#画面出力確認用
+#for interval, df in multi_df.items():
+#    print(f"\n=== {interval} の最新5本 ===")
+#    print(df.tail(5))        
 
-# AI戦略判断を実施（SMA情報含めて渡す）
+#CSV出力確認用
+for interval, df in multi_df.items():
+    filename = f"data/usd_jpy_{interval}.csv"
+    df.to_csv(filename, index=False)
+
+#AI戦略判断を実施
 body = get_trade_signal(multi_df)
 
+#分析結果をメール送信
 send_email_notification("AI分析",body)
 
 
